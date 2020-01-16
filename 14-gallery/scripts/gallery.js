435a7811b1d8fd9eb09d67cc811b2b6e6ebf467b
Pierre Bretéché <pbreteche@dawan.fr>
2020-01-16 14:33:06 +0100

    création du composant Gallery

--- 14-gallery/scripts/gallery.js
+++ 14-gallery/scripts/gallery.js
@@ -2,18 +2,20 @@
 
 import { IMAGES } from './images.js';
 
-document.addEventListener('DOMContentLoaded', function() {
+export class Gallery {
 
-    const navBar = document.querySelector('nav');
-    const bigImage = document.querySelector('article > img');
-
-    const navList = navBar.children[0];
-    navList.innerHTML = IMAGES.reduce((allHtml, path) => allHtml+`<li><a><img src="img/${path}"></a></li>`, '')
-
-    const navLinks = navBar.querySelectorAll('a');
-    for (const link of navLinks) {
-        link.addEventListener('click', function() {
-            bigImage.src = this.children[0].src;
-        });
+    start() {
+        const navBar = document.querySelector('nav');
+        const bigImage = document.querySelector('article > img');
+    
+        const navList = navBar.children[0];
+        navList.innerHTML = IMAGES.reduce((allHtml, path) => allHtml+`<li><a><img src="img/${path}"></a></li>`, '')
+    
+        const navLinks = navBar.querySelectorAll('a');
+        for (const link of navLinks) {
+            link.addEventListener('click', function() {
+                bigImage.src = this.children[0].src;
+            });
+        }
     }
-});
+}
---
+++ 14-gallery/scripts/main.js
@@ -0,0 +1,9 @@
+'use strict';
+
+import { Gallery } from './gallery.js';
+
+document.addEventListener('DOMContentLoaded', function() {
+    const gallery = new Gallery();
+
+    gallery.start();
+});
