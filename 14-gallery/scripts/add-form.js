6e95ba9ec5af0fa794bf945df9ff961a00da7f48
Pierre Bretéché <pbreteche@dawan.fr>
2020-01-17 15:49:20 +0100

    autres requêtes HTTP

--- 14-gallery/scripts/add-form.js
+++ 14-gallery/scripts/add-form.js
@@ -27,11 +27,43 @@ export class AddForm {
                     detail: {src: reader.result}
                 });
                 this.dispatchEvent(pictureCreatedEvent);
+
+                // envoi du contenu du fichier en JSON
+                // nous avons besoin du retour du FileReader
+                fetch('url où envoyer les données', {
+                    method: 'POST',
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify({ name: newFile.name, data: reader.result})
+                }).then(response => console.log(response))
+                .catch(error => console.log(error))
             });
 
 
             reader.readAsDataURL(newFile);
             this.classList.add('hidden');
+
+            // envoi du contenu du fichier en form-data
+            // nous utilisons directement l'objet FormData
+            fetch('url où envoyer les données', {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'multipart/form-data'
+                },
+                body: new FormData(this)
+            }).then(response => console.log(response))
+            .catch(error => console.log(error))
+
+            // la même chose avec XMLHttpRequest
+            const xhr = new XMLHttpRequest();
+            xhr.open('POST', 'url où envoyer les données');
+            xhr.setRequestHeader('Content-type', 'multipart/form-data');
+            xhr.addEventListener('load', function(){
+                console.log(this);
+            })
+
+            xhr.send(new FormData(this));
         });
     }
 }
