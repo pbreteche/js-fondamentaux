5d1b242e96fe25f6f129ac77bae2ee033e4c100c
Pierre Bretéché <pbreteche@dawan.fr>
2020-01-17 16:58:00 +0100

    géo-localisation

--- 14-gallery/scripts/main.js
+++ 14-gallery/scripts/main.js
@@ -2,6 +2,19 @@
 
 import { Gallery } from './gallery.js';
 
+navigator.geolocation.getCurrentPosition(
+    location => {
+        console.log(location);
+        console.log(location.coords);
+    },
+    error => console.log(error)
+)
+
+// accès au web storage
+// stockage possible via des chaînes de caractères identifiées par des clés
+localStorage.setItem('key', { a: 'aaa', b: 'bbb'})
+sessionStorage.setItem('test2', JSON.stringify({ a: 'aaa', b: 'bbb'}))
+
 document.addEventListener('DOMContentLoaded', function() {
     const gallery = new Gallery('#gallery');
 
